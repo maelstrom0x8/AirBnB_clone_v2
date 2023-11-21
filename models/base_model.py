57b290a9c34c9b53092a4d11ad017fa01696209e
Chidimma <chidimmamofodum@gmail.com>
2023-11-21 12:40:03 -0500

    mapping basemodel with orm

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, DateTime
+from models.engine.file_storage import FileStorage
 import uuid
 from datetime import datetime
 
 
+def id_generator() -> str:
+    _id = uuid.uuid4()
+    return str(_id)
+
+
+Base = declarative_base()
+
+
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, default=id_generator)
+    created_at = Column(DateTime(), default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime(), default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,7 +28,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,14 +45,22 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
+        orm_name = '_sa_instance_state'
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if orm_name in dictionary:
+            del (dictionary[orm_name])
         return dictionary
+
+    def delete(self):
+        fs = FileStorage()
+        fs.delete(self)
