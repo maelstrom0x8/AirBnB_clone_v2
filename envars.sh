125c7429cb49052fb74778fdd44e78be3b1751db
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-11-23 04:39:38 +0100

    Minor changes

---
+++ envars.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export HBNB_ENV=dev
+export HBNB_MYSQL_DB="hbnb_${HBNB_ENV}_db"
+export HBNB_TYPE_STORAGE=db
+export HBNB_MYSQL_HOST='127.0.0.1'
+export HBNB_MYSQL_PWD="hbnb_${HBNB_ENV}_pwd"
+export HBNB_MYSQL_USER="hbnb_${HBNB_ENV}"
+export HBNB_DB_PORT=33042
+
+alias dc='docker-compose'
