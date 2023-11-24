c04dcd10d1349c473119fe62babe0cc04492ca15
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-11-25 00:27:13 +0100

    Update .gitignore

--- envars.sh
+++ envars.sh
@@ -2,12 +2,12 @@
 
 
 export HBNB_ENV=dev
-export HBNB_MYSQL_DB=sceptre
+export HBNB_MYSQL_DB="hbnb_${HBNB_ENV}_db"
 export HBNB_TYPE_STORAGE=db
 export HBNB_MYSQL_HOST='127.0.0.1'
-export HBNB_MYSQL_PWD=aeflheim
-export HBNB_MYSQL_USER=fargo
-export HBNB_DB_PORT=42343
+export HBNB_MYSQL_PWD="hbnb_${HBNB_ENV}"
+export HBNB_MYSQL_USER="hbnb_${HBNB_ENV}"
+export HBNB_DB_PORT=56564
 
 alias dc='docker-compose'
 alias pc=pycodestyle
